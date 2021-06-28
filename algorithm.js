408cc41fe606e5acfe3bb9e3b03930162a902e15
Ali <alimamdouhsalem@gmail.com>
2021-06-28 15:44:35 +0200

    Checkpoint 6

---
+++ algorithm.js
@@ -0,0 +1,21 @@
+var str="Hello World."
+var numWords=0;
+var numVowls=0;
+str.toLowerCase();
+for(var i=0; i<str.length;i++){
+    if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'||str.charAt(i)=='y'){
+        numVowls++;
+    }
+    if(str.charAt(i)==' '){
+        numWords++
+       
+    
+    }
+    
+    
+}
+numWords++
+var strSize= i;
+console.log(numVowls)
+console.log(numWords)
+console.log(strSize)
